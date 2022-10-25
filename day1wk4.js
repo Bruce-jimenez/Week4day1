be351b1459db3f908e80f338d9b07a502fc8fc23
Bruce-jimenez <jimenezbruce2022@gmail.com>
2022-10-25 13:37:03 -0400

    function

---
+++ day1wk4.js
@@ -0,0 +1,11 @@
+const logNumbers = () =>{
+    for(i = 0; i < 20; i++){
+        console.log(i)
+    }
+}
+const getEven = () => {
+    for(let i = 0; i <= 200; i ++){
+        if(i % 2 == 0 && i !== 0) {
+            console.log(i)}
+        }
+}
