0ecc89339ffe9c09886a2862c2d3fe4fd9eaf89a
Bruce-jimenez <jimenezbruce2022@gmail.com>
2022-10-25 14:07:58 -0400

    Yell at the Ninja Turtles answered

--- day1wk4.js
+++ day1wk4.js
@@ -1,11 +1,89 @@
-const logNumbers = () =>{
-    for(i = 0; i < 20; i++){
-        console.log(i)
-    }
+////////////////////////////////
+// Easy Going
+////////////////////////////////
+// const logNumbers = () =>{
+//     for(i = 0; i < 20; i++){
+//         console.log(i)
+//     }
+// }
+
+////////////////////////////////
+// Get Even
+// ////////////////////////////////
+// const getEven = () => {
+//     for(let i = 0; i <= 200; i ++){
+//         if(i % 2 == 0 && i !== 0) {
+//             console.log(i)}
+//         }
+// }
+
+////////////////////////////////
+// Fizz Buzz
+////////////////////////////////
+// const getMyNumbers = () => {
+//     let num;
+//     for(let i=1; i < 101; i++){
+//         num = i;
+
+//     if(num%3===0 && num%5===0){
+//         console.log('Fizzbuzz');
+//     }
+//     else if(num%3===0){
+//         console.log('Fizz')
+//     }
+//     else if(num%5===0){
+//         console.log('Buzz')
+//     }
+//     else{
+//         console.assert.log(num);
+//     }
+//     }
+// }
+
+////////////////////////////////
+// Wild Wild Life
+////////////////////////////////
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+// plantee[2] += 1;
+// console.log(plantee);
+
+// wolfy[3] = "Gotham City";
+
+// dart[3] += "Hawkings";
+// console.log(dart);
+
+// wolfy.shift();
+// wolfy.unshift("Gameboy");
+// console.log(wolfy);
+
+////////////////////////////////
+// Yell at the Ninja Turtles
+////////////////////////////////
+const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
+for (const i of ninjaTurtles){
+    console.log(i.toUpperCase())
 }
-const getEven = () => {
-    for(let i = 0; i <= 200; i ++){
-        if(i % 2 == 0 && i !== 0) {
-            console.log(i)}
-        }
-}
+
+////////////////////////////////
+// Methods, Revisited
+////////////////////////////////
+
+
+////////////////////////////////
+// Where is Waldo
+////////////////////////////////
+
+
+////////////////////////////////
+//  Excited Kitten
+////////////////////////////////
+
+
+////////////////////////////////
+//  Find the Median
+////////////////////////////////
